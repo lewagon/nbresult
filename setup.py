62663acd9702f26cc2d576ab62a07e5b134f36b9
Oliver Giles <ogiles@email.com>
2023-07-18 10:01:28 +0100

    Update setup.py
    Co-authored-by: Gaëtan Manchon <34584935+gmanchon@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 with open('requirements.txt') as f:
-    required = f.read().splitlines()
+    required = [c.strip() for c in f.readlines()]
 
 setup(name='nbresult',
       version='0.0.9',
